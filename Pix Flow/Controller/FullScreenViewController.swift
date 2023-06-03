cf0b73143a051193d97503977ac86a866c893f7a
Andrea Bottino <bottinoandrea93@gmail.com>
2023-06-03 15:53:32 +0100

    Implemented image download

--- Pix Flow/Controller/FullScreenViewController.swift
+++ Pix Flow/Controller/FullScreenViewController.swift
@@ -18,4 +18,24 @@ class FullScreenViewController: UIViewController {
         super.viewDidLoad()
         imageView.image = image
     }
+    
+    @IBAction func downloadImage(_ sender: UIButton) {
+        
+        UIImageWriteToSavedPhotosAlbum(image!, self, #selector(savedImage), nil)
+    }
+    
+    @objc func savedImage(_ image: UIImage, error: Error?, context: UnsafeMutableRawPointer?) {
+        if let error = error {
+            print(error)
+            return
+        } else {
+            downloadMessage()
+        }
+    }
+    
+    func downloadMessage() {
+        let downloadMessage = UIAlertController(title: "Download complete", message: "The image has been saved in your Photos app", preferredStyle: .alert)
+        downloadMessage.addAction(UIAlertAction(title: "OK", style: .default))
+        present(downloadMessage, animated: true)
+    }
 }
